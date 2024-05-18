d6410f8599c02b89c3dc53c3d46566f8a56073d1
vinod <21r21a3344@mlrinstitutions.ac.in>
2024-05-18 19:32:23 +0530

    problem added

---
+++ base/B_Fix_You.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<char>> v(n, vector<char>(m));
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                cin >> v[i][j];
+            }
+        }
+        int ans = 0;
+        for(int i = 0; i < n; i++) {
+            if(v[i][m-1] == 'R') {
+                ans++;
+            }
+        }
+        for(int i = 0; i < m; i++) {
+            if(v[n-1][i] == 'D') {
+                ans++;
+            }
+        }
+        cout << ans << endl;
+    }
+
+
+    return 0;
+}
